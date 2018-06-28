a5793ae4ed5d587df6c38d06e156dd651aaa6992
Oscar Wong <wong.oscar67@gmail.com>
2018-06-27 20:27:53 -0600

    added a file for the new canvas component

---
+++ src/Canvas.js
@@ -0,0 +1,12 @@
+//this will be the new canvas component to replace the current one
+//it should keep as much functionality as possible in that it should:
+//allow the user to change colours and undo
+//it should support live-streaming of drawings w/ minimal bandwidth hit - ToDo: figure out how that'll work
+//it should be easily cleared
+import React, { Component } from 'react';
+
+class Canvas extends Component {
+
+}
+
+export default Canvas;
